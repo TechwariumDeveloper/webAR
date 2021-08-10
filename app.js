28052bc9a78ca1249fc913113b4aa279803130a8
TechwariumDeveloper <59221075+TechwariumDeveloper@users.noreply.github.com>
2021-08-10 19:50:43 +0530

    changes

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-import * as THREE from '/libs/three125/three.module.js';
-import { GLTFLoader } from '/libs/three/jsm/GLTFLoader.js';
-import { RGBELoader } from '/libs/three/jsm/RGBELoader.js';
-import { ARButton } from '../../libs/ARButton.js';
-import { LoadingBar } from '/libs/LoadingBar.js';
+import * as THREE from '../webAR/libs/three125/three.module';
+import { GLTFLoader } from '../webAR/libs/three/jsm/GLTFLoader.js';
+import { RGBELoader } from '../webAR/libs/three/jsm/RGBELoader.js';
+import { ARButton } from '../webAR/libs/ARButton.js';
+import { LoadingBar } from '../webAR/libs/LoadingBar.js';
 
 class App{
 	constructor(){
@@ -12,7 +12,7 @@ class App{
         this.loadingBar = new LoadingBar();
         this.loadingBar.visible = false;
 
-		this.assetsPath = '/assets/ar-shop/';
+		this.assetsPath = '../webAR/assets/ar-shop/';
         
 		this.camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 20 );
 		this.camera.position.set( 0, 1.6, 0 );
@@ -95,7 +95,7 @@ class App{
         
         const self = this;
         
-        loader.load( '/assets/hdr/venice_sunset_1k.hdr', ( texture ) => {
+        loader.load( '../webAR/assets/hdr/venice_sunset_1k.hdr', ( texture ) => {
           const envMap = pmremGenerator.fromEquirectangular( texture ).texture;
           pmremGenerator.dispose();
 
